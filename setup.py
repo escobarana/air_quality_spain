afe498b14dcf8bf1c26b95f13437a058d29c5686
Ana Escobar <anaescobar8@gmail.com>
2022-10-10 22:17:57 +0200

    Fix: Updating setup.py file

--- setup.py
+++ setup.py
@@ -9,22 +9,31 @@ ver_path = convert_path('crawler/__init__.py')
 with codecs.open(ver_path, 'rb', 'utf8') as ver_file:
     exec(ver_file.read(), main_ns)
 
-install_requires = ['requests~=2.28.1']
+install_requires = ['requests~=2.28.1',
+                    'pymongo~=4.2.0',
+                    'celery~=5.2.6',
+                    'Django~=4.1.2',
+                    'pandas~=1.5.0',
+                    'numpy~=1.23.3',
+                    'streamlit~=1.13.0',
+                    'pydeck==0.7.1']
 
 setup(
     name=main_ns['__name__'],
     version=main_ns['__version__'],
     author=main_ns['__author__'],
     author_email=main_ns['__author_email__'],
-    description='API Crawler',
+    description='OpenAQ API Crawler',
     license='',
     platforms=['Any'],
-    keywords=['mongodb', 'openaq'],
+    keywords=['pymongo', 'openaq', 'streamlit', 'django'],
     url='',
     packages=['crawler',
               'crawler.wrappers',
               'crawler.helpers',
-              'crawler.wrappers.openaqapi'],
+              'crawler.wrappers.openaqapi',
+              'tasks',
+              'writers'],
     zip_safe=False,
     include_package_data=True,
     install_requires=install_requires,
